53acc6cc0b02c94fb7ccfbac448df8bcdea0cdef
Shiam <snshiam9819@gmail.com>
2020-03-23 22:11:47 +0600

    BICE-18 App

--- app/src/main/java/com/shabiruzzaman_shiam/bice_18/fbgroup.java
+++ app/src/main/java/com/shabiruzzaman_shiam/bice_18/fbgroup.java
@@ -3,6 +3,7 @@ package com.shabiruzzaman_shiam.bice_18;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,6 +14,8 @@ public class fbgroup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_fbgroup);
 
         webView =findViewById(R.id.fb);
